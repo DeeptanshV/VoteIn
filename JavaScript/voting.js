69e145709416a15197f6cfd3e2d3b93138ba7502
Parth Sundarka <77690791+parthusun8@users.noreply.github.com>
2023-07-07 20:29:15 +0530

    voting js updated

--- JavaScript/voting.js
+++ JavaScript/voting.js
@@ -1,14 +1,37 @@
-document.onload = getContainer();
+const url = window.location.href;
+const urlParams = new URLSearchParams(url);
+console.log(urlParams.get('name'));
+console.log(urlParams.get('email'));
+console.log(document.getElementsByClassName('name'));
+document.getElementsByClassName('name')[0].innerText = urlParams.get('name');
+document.getElementsByClassName('email')[0].innerText = urlParams.get('email');
+const participants= [
+   {
+      name : "BJD",
+      id : "1"
+   },
+   {
+      name : "AAP",
+      id : "08"
+   },
+   {
+      name : "BJP",
+      id : "ytgfv"
+   }
+]
+
+getContainer();
 
 function getContainer() {
+   
    const container1 = document.querySelector('.container1');
    console.log(container1);
-   for (let i = 0; i < 3; i++) {
+   for (let i = 0; i < participants.length; i++) {
       const card = 
          `<div class="card" style="width: 18rem;height: 35rem">
             <img src="voting pic.jpg" class="card-img-top" alt="p1" />
             <div class="card-body">
-               <h5 class="card-title">Participant 1</h5>
+               <h5 class="card-title">${participants[i].name}</h5>
                <p class="card-text"></p>
                <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex voluptate totam dolor tempora esse excepturi
                   doloribus odio atque repellendus ad, recusandae earum laborum tempore nostrum, voluptatum ea veritatis magnam
@@ -18,27 +41,16 @@ function getContainer() {
          </div>`;
    
          container1.innerHTML += card;
-   } 
+   }
    btnListener();  
 }
 
 function btnListener(){
-   for(let i=0;i<3;i++){
+   for(let i=0;i<participants.length;i++){
       let voteButton = document.querySelector(`#vote${i}`);
       console.log(voteButton);
       voteButton.addEventListener("click",function(){
-         window.location.href = 'thanks.html';
+         window.location.href = 'thanks.html?votedTo=' + participants[i].name;
       });
    }
 }
-
-
-
-
-const url = window.location.href;
-const urlParams = new URLSearchParams(url);
-console.log(urlParams.get('name'));
-console.log(urlParams.get('email'));
-console.log(document.getElementsByClassName('name'));
-document.getElementsByClassName('name')[0].innerText = urlParams.get('name');
-document.getElementsByClassName('email')[0].innerText = urlParams.get('email');
