d076862f169ba2969d844cdf0ace5007cbba37f4
Deeptansh Verma <deeptansh2996@gmail.com>
2023-07-07 15:35:24 +0530

    Initial Commit

---
+++ JavaScript/voting.js
@@ -0,0 +1,8 @@
+let voteButton = document.querySelector("#first-participant"); 
+let container=document.querySelector('.container');
+voteButton.addEventListener("click",function(){
+   document.querySelector("h2").innerText="Thanks for voting"
+})
+
+
+
