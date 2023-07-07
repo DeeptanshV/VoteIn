c92888bcc52372b1a9c8ce0c21e8f133ada40a1e
Parth Sundarka <77690791+parthusun8@users.noreply.github.com>
2023-07-07 15:57:48 +0530

    login and register page done

---
+++ index.js
@@ -0,0 +1,29 @@
+function register() {
+    const passInput = document.getElementById('pass');
+    console.log(passInput.value);
+    const emailInput = document.getElementById('email');
+    console.log(emailInput.value);
+    const fnameInput = document.getElementById('fname');
+    console.log(fnameInput.value);
+
+    if(passInput.value == '' || emailInput.value == '' || fnameInput.value == '') {
+        alert('Please fill in all fields');
+        return;
+    }
+    alert('Hello World');
+    window.location.href = 'votepage.html?fname=' + fnameInput.value + '&email=' + emailInput.value;
+}
+
+
+function login(){
+    const emailInput = document.getElementById('emailLogin');
+    const passInput = document.getElementById('passLogin');
+
+    if(emailInput.value == '' || passInput.value == '') {
+        alert('Please fill in all fields');
+        return;
+    }
+    const name = 'John';
+    alert('Hello World');
+    window.location.href = 'votepage.html?email=' + emailInput.value + '&name=' + name;
+}
