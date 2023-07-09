653ffa0b6bfc6e19399801be0cc7010a40397d1b
Deeptansh Verma <75324676+DeeptanshV@users.noreply.github.com>
2023-07-09 18:48:41 +0530

    Update ID of participants

--- JavaScript/voting.js
+++ JavaScript/voting.js
@@ -11,17 +11,17 @@ const participants = [
    {
       name: "BJD",
       id: "1",
-      "text" : "The Biju Janata Dal  is an Indian regional political party with great influence in the state of Odisha. It was founded by the former minister of mines and minerals of the Republic of India Naveen Patnaik on 26 December 1997 as a breakaway faction from the Janata Dal.[8] The BJD is led by its founder as president of the party. The headquarters of the party is located in Forest Park, Bhubaneswar."
+      "text": "The Biju Janata Dal  is an Indian regional political party with great influence in the state of Odisha. It was founded by the former minister of mines and minerals of the Republic of India Naveen Patnaik on 26 December 1997 as a breakaway faction from the Janata Dal.[8] The BJD is led by its founder as president of the party. The headquarters of the party is located in Forest Park, Bhubaneswar."
    },
    {
       name: "AAP",
-      id: "08",
-      "text" : "The Aam Aadmi Party  is a political party in India. It was founded in November 2012 by Arvind Kejriwal and his then-companions,[25] following the 2011 Indian anti-corruption movement. AAP is currently the governing party in the Indian state of Punjab and the union territory of Delhi. On 10 April 2023, AAP was officially granted the status of National party by ECI.The party's election symbol is a broom."
+      id: "2",
+      "text": "The Aam Aadmi Party  is a political party in India. It was founded in November 2012 by Arvind Kejriwal and his then-companions,[25] following the 2011 Indian anti-corruption movement. AAP is currently the governing party in the Indian state of Punjab and the union territory of Delhi. On 10 April 2023, AAP was officially granted the status of National party by ECI.The party's election symbol is a broom."
    },
    {
       name: "BJP",
-      id: "ytgfv",
-      "text" : "The Bharatiya Janata Party  is a political party in India, and one of the two major Indian political parties alongside the Indian National Congress.[38] Since 2014, it has been the ruling political party in India under Narendra Modi, the incumbent Indian prime minister.[39] The BJP is aligned with right-wing politics, and its policies adhere to Hindutva, a Hindu nationalist ideology.[40][41] It has close ideological and organisational links to the Rashtriya Swayamsevak Sangh (RSS).[42] As of March 2023, it is the country's biggest political party in terms of representation in the Parliament of India as well as state legislatures."
+      id: "3",
+      "text": "The Bharatiya Janata Party  is a political party in India, and one of the two major Indian political parties alongside the Indian National Congress.[38] Since 2014, it has been the ruling political party in India under Narendra Modi, the incumbent Indian prime minister.[39] The BJP is aligned with right-wing politics, and its policies adhere to Hindutva, a Hindu nationalist ideology.[40][41] It has close ideological and organisational links to the Rashtriya Swayamsevak Sangh (RSS).[42] As of March 2023, it is the country's biggest political party in terms of representation in the Parliament of India as well as state legislatures."
    }
 ]
 
